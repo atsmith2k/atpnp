ee62df51e8a06eed8d5499cfc8bed7bff1199ca6
Ashton Smith <atsmith2k@gmail.com>
2022-05-18 21:10:03 -0400

    firstpush

---
+++ js/poop.js
@@ -0,0 +1,13 @@
+
+function openGarage() {
+    let container = document.querySelector('.container');
+
+    container.classList.add('animate__animated');
+    container.classList.add('animate__slideOutUp');
+
+    document.addEventListener("keydown", (event) => {
+        
+        container.classList.remove('animate__slideOutUp');
+        container.classList.add('animate__slideInDown');
+    });
+}
