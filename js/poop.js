9c51589a2f2b92133754487c5d57978d9b200f30
Ashton Smith <atsmith2k@gmail.com>
2022-05-19 18:35:57 -0400

    store

--- js/poop.js
+++ js/poop.js
@@ -1,5 +1,5 @@
 
-function openGarage() {
+async function openGarage() {
     let container = document.querySelector('.container');
 
     container.classList.add('animate__animated');
@@ -9,4 +9,19 @@ function openGarage() {
         container.classList.remove('animate__slideOutUp');
         container.classList.add('animate__slideInDown');
     });
+    await delay(600);
+    window.location = './shop.html'
+}
+
+function delay(milliseconds){
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
+
+function insideGarage() {
+    let container = document.querySelector('.container').style.display = 'none';
+    let products = document.getElementById('product-component-1652968921100');
+    products.style.display = "block";
+    console.log(products)
 }
